ff621b1db16c92a060230c84eee9152f7a418418
Vijaya Hasini Malyala <malyalavijayahasini@gmail.com>
2026-09-03 00:11:57 +0530

    Time: 0 ms (100%), Space: 44.1 MB (68.42%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -0,0 +1,15 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode nxt = curr.next; 
+            curr.next = prev;         
+            prev = curr;              
+            curr = nxt;               
+        }
+
+        return prev;
+    }
+}
